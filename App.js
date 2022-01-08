4df5e1b36465e6bb0532019d2c2abb2097c3f786
vexleet <abda70226@gmail.com>
2022-01-08 16:49:31 +0100

    add statusbar component to keep statusbar visible on mobile devices

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Home from './screens/Home';
 import { NativeRouter, Route, Routes } from 'react-router-native';
 import ChampDetails from './screens/ChampDetails';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <NativeRouter>
+      <StatusBar />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/champion/:championName' element={<ChampDetails />} />
